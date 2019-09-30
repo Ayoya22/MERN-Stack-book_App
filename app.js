c1443e87d2c2794f41a05fdccd1fb2d1c2dc6b40
Ayoya22 <djoukamsimplice@gmail.com>
2019-09-30 06:35:33 +0100

    updated stack app

--- app.js
+++ app.js
@@ -2,8 +2,10 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
+//pass:ns6PKRwCHBaRXrkB
 //Connect to database(mongo)
 connectDB();
+
 app.get('/', (req,res) => res.send('Hi there, welcome to your online library'));
 
 const port = process.env.PORT || 8082;
