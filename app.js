f83139b77581fa3d25a1091b644cef6f7d2d1671
Ayoya22 <djoukamsimplice@gmail.com>
2019-09-30 04:45:59 +0100

    Completed server configuration

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 // configuring the server
 const express = require('express');
+
+const app = express();
+
+app.get('/', (req,res) => res.send('Hi there, welcome to your online library'));
+
+const port = process.env.PORT || 8082;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
