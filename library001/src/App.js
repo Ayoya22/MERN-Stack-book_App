69c91d890cd3adf8a877176a89698a39cc25b2d5
Ayoya22 <djoukamsimplice@gmail.com>
2019-10-02 03:03:02 +0100

    Dealing with front-end, co,pleting app.js
    Signed-off-by: Ayoya22 <djoukamsimplice@gmail.com>

--- library001/src/App.js
+++ library001/src/App.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
+import CreateBook from './components/CreateBook';
+import ShowBookList from './components/ShowBookList';
+import ShowBookDetails from './components/ShowBookDetails';
+import UpdateBookInfo from './components/UpdateBookInfo';
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Route exact path='/' component={ShowBookList} />
+                    <Route path='/create-book' component={CreateBook} />
+                    <Route path='/edit-book/:id' component={UpdateBookInfo} />
+                    <Route path='/show-book/:id' component={ShowBookDetails} />
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
